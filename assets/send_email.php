0d4e8d2fa1f4253784c860d635a3eed51a92e3f8
Meftah Uddin <94321097+meftaH416@users.noreply.github.com>
2024-12-23 17:47:19 -0600

    Add files via upload
    add php file for sending message

---
+++ assets/send_email.php
@@ -0,0 +1,22 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Retrieve form data
+    $name = htmlspecialchars($_POST['name']);
+    $email = htmlspecialchars($_POST['email']);
+    $message = htmlspecialchars($_POST['message']);
+
+    // Email details
+    $to = "mucd7@umsystem.edu";
+    $subject = "New Message from $name";
+    $headers = "From: $email\r\n";
+    $headers .= "Reply-To: $email\r\n";
+    $body = "Name: $name\nEmail: $email\n\nMessage:\n$message";
+
+    // Send the email
+    if (mail($to, $subject, $body, $headers)) {
+        echo "Message sent successfully!";
+    } else {
+        echo "Error sending message.";
+    }
+}
+?>
